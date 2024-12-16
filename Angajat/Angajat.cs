0709df9a42c72eca89d118e4259954e65f726b3c
Victor Lanciu <wlanciu@gmail.com>
2024-12-16 14:39:14 +0200

    Am adaugat clasa Angajat si am apelat-o in main.

---
+++ Angajat/Angajat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Angajat
+{
+    public class Angajat
+    {
+        public string Nume;
+        public int Salariu;
+        public string Departament;
+
+        public Angajat(string nume, int salariu, string departament)
+        {
+            this.Nume = nume;
+            this.Salariu = salariu;
+            this.Departament = departament;
+        }
+        public void AfisareDetalii()
+        {
+            Console.WriteLine($"Numele angajatului:{Nume}, Salariul Angajatului: {Salariu}, Departamentul angajatului: {Departament}");
+        }
+    }
+}
---
+++ Angajat/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Angajat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Angajat Igor = new Angajat("Igor", 1000, "IT");
+            Igor.AfisareDetalii();
+            Angajat Gabi = new Angajat("Gabi", 2000, "Marketing");
+            Gabi.AfisareDetalii();
+            Angajat Victor = new Angajat("Victor", 1200, "Departament Juridic");
+            Victor.AfisareDetalii();
+        }
+    }
+}
